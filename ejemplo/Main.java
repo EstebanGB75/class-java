afa0913f3a6fa0dfd2f35ff819bf6ae07f590ecb
estebitan9309 <estebitan9309@gmail.com>
2017-07-19 11:09:33 -0400

    se agrego la resta

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -6,10 +6,17 @@ public class Main {
     System.out.println("Hola");
     int s = suma(20,30);       
     System.out.println(s);
+    int r = resta(25,17);
+    System.out.println(r);
+    
   }
       
    public static int suma (int n1,int n2){
      return  n1+n2 ;
      
     }
+  public static int resta(int n1, int n2){
+   
+    return n1-n2 ;
+  }
 }
